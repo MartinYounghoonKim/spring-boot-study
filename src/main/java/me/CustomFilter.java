029f88399435f6733188c0b530e8189f620d65a7
martinyounghoonkim <hoons0131@gmail.com>
2020-04-01 23:30:28 +0900

    SevletLister 와 Filter 등록

---
+++ src/main/java/me/CustomFilter.java
@@ -0,0 +1,22 @@
+package me;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class CustomFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("Filter init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("Filter doFilter");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Filter destroy");
+    }
+}
